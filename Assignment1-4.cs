219b363a6f687b6febe88fa7e1aa5dbee13dab62
susela <suselakishore23@gmail.com>
2025-01-16 10:41:20 +0530

    Add files via upload

---
+++ Assignment1-4.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int y = -5; y <= 5; y++)
+            {
+                int x = (y * y) + (2 * y) + 1;
+                Console.WriteLine($"y = {y}, x = {x}");
+            }
+        }
+    }
+}
---
+++ Assignment1.cs
@@ -0,0 +1,43 @@
+﻿namespace Assignment1
+{
+    internal class Assignment1
+    {
+        static void Main(string[] args)
+        {
+            string originalUserName = "admin";
+            string originalPassword = "admin123";
+
+            string userName, password;
+            int attempt = 0;
+            const int maxAttempts = 3;
+
+            //user authentication loop
+            while (attempt < maxAttempts)
+            {
+                Console.Write("Enter user name:");
+                userName = Console.ReadLine();
+
+                Console.Write("Enter password:");
+                password = Console.ReadLine();
+
+                if (userName == originalUserName && password == originalPassword)
+                {
+                    Console.WriteLine("Login Successfully!");
+                    break;
+                }
+
+                else
+                {
+                    attempt++;
+                    Console.WriteLine($"Invalid credentials.Attempt{attempt} out of {maxAttempts}");
+                }
+                if (attempt == maxAttempts)
+                {
+                    Console.WriteLine("Invalid Credentials. Exceeds 3 attempts!");
+                }
+                Console.WriteLine("Press any key to exit..");
+                Console.ReadKey();
+            }
+        }
+    }
+}
